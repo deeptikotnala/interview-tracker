a05a71f1c9fb21226f7a51e8fd8ab36c0f0cafdf
deepti.kotnala@gmail.com <deepti.kotnala@gmail.com>
2018-09-26 00:40:57 -0400

    Routing completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from 'src/dashboard/dashboard.component';
+import { ApplicationComponent } from './application/application.component';
+import { InterviewsComponent } from './interviews/interviews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ApplicationComponent,
+    InterviewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+         path: 'dashboard',
+         component: DashboardComponent
+      },
+      {
+        path: 'applications',
+        component: ApplicationComponent
+     },
+     {
+      path: 'interviews',
+      component: InterviewsComponent
+   },
+   ])
   ],
   providers: [],
   bootstrap: [AppComponent]
